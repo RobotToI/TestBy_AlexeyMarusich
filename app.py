5c26fb19a664d37a087be4f2acceda7bcb029565
Mikita Tratsiuk <trotsyuk2001@gmail.com>
2022-11-23 18:55:00 +0300

    chore(v2): refactor application, visability of async work, add new iterators

--- app.py
+++ app.py
@@ -1,64 +1,79 @@
 import sys
 from typing import List
+
 import requests
-
-REQUEST_METHODS = {"GET": requests.get,
-                    "OPTIONS": requests.options,
-                    "HEAD": requests.head,
-                    "POST": requests.post,
-                    "PATCH": requests.patch,
-                    "PUT": requests.put,
-                    "DELETE": requests.delete
-                    }
+import validators
 
 
-def get_info(url: str, avaliable_methods: List[str]):
-    result = dict()
-    for method in avaliable_methods:
-        related_function = REQUEST_METHODS.get(method)
-        status_code = related_function(url).status_code
-        if status_code != 405:
-            result[method] = status_code
+class MatchedRequests:
+    __slots__ = "available_methods"
 
-    return result 
+    _method_matching = {
+        "GET": requests.get,
+        "HEAD": requests.head,
+        "POST": requests.post,
+        "PATCH": requests.patch,
+        "OPTIONS": requests.options,
+        "DELETE": requests.delete,
+    }
+
+    def __init__(self, available_methods: List[str]):
+        self.available_methods = [
+            (k, value)
+            for k, value in MatchedRequests._method_matching.items()
+            if k in available_methods
+        ]
+
+    def __iter__(self):
+        for method_name, method_function in self.available_methods:
+            yield method_name, method_function
+        return
 
 
-def validate_http_protocol_usage(url:str):
-    if url.startswith('http://') or url.startswith('https://'):
-        return True
-    return False
+class MethodResponseConfig:
+    __slots__= 'url', 'available_methods'
+    def __init__(self, url: str):
+        self.url = url
 
-def check_all_methods(url: str):
-    verbs_avaliable = requests.options(url).headers.get('allow')
-    if not verbs_avaliable:
-        return {url: "No HTTP methods availiable"}
-    avaliable_methods = verbs_avaliable.split(', ')
-    result_responces = get_info(url, avaliable_methods)
+    def get_method_responses(self):
+        method_responses = dict()
+        if not self.available_methods:
+            return {self.url: "There is no available methods"}
+        for method_name, method_function in MatchedRequests(self.available_methods):
+            method_responses.update(
+                {method_name: method_function(self.url).status_code}
+            )
 
-    return {url: result_responces}
+        return {self.url: method_responses}
+
+    def set_available_methods(self):
+        options_allows = requests.options(self.url).headers.get("allow")
+        if not options_allows:
+            self.available_methods = None
+        else:
+            self.available_methods = options_allows.split(", ")
 
 
-def read_lines_from_stdin():
-    result_lines = set()
-    for line in sys.stdin 
-        if 'q' == line.rstrip().lower():
-            break
-        result_lines.add(line.rstrip())
-        print(f"{line.rstrip()} has been taken to processing")
-    print("Processing...\n")
-    return result_lines
+def url_stdin_reader():
+    """Function read lines from stdin and collects result in result set"""
+    for line in sys.stdin:
+        clear_line = line.rstrip().lower()
+        if not clear_line:
+            return
+        if not validators.url(clear_line):
+            continue
+        yield clear_line
+
 
 def main():
-    urls = read_lines_from_stdin()
-    output_json = dict()
-    for url in urls:
-        if validate_http_protocol_usage(url):
-            output_json.update(check_all_methods(url))
-        else:
-            output_json.update({url: "Not valid url"})
-    
+    result = list()
+    for url in url_stdin_reader():
+        config = MethodResponseConfig(url)
+        config.set_available_methods()
+        result.append(config.get_method_responses())
     from pprint import pprint
-    pprint(output_json)
+
+    pprint(result)
 
 
 if __name__ == "__main__":
